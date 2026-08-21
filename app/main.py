6b8ffa87f38141a8037f329aad365942fcd33cec
Đạt  Nguyễn <datllkk456@gmail.com>
2026-08-21 09:37:15 +0700

    buổi 2 task 1: POST /auth/register: tạo tài khoản, kiểm tra email trùng, validate dữ liệu đầu vào.

--- app/main.py
+++ app/main.py
@@ -6,12 +6,17 @@ from app.models.role import Role
 from app.models.research_project import ResearchProject
 from app.models.research_member import ResearchMember
 from app.models.research_task import ResearchTask
+from app.routers.auth import router 
 
 app = FastAPI(
     title="Research Group Management API"
 )
 
+# Tạo bảng
 Base.metadata.create_all(bind=engine)
+
+# Đăng ký Authentication router
+app.include_router(router)
 
 @app.get("/")
 def root():
--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,6 +1,5 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-
 from app.db.database import get_db
 from app.models.user import User
 from app.schemas.user import UserCreate, UserResponse
@@ -11,16 +10,10 @@ router = APIRouter(
     tags=["Authentication"]
 )
 
-
 @router.post("/register", response_model=UserResponse)
-def register(
-    user_data: UserCreate,
-    db: Session = Depends(get_db)
-):
+def register(user_data: UserCreate, db: Session = Depends(get_db)):
     # Kiểm tra email đã tồn tại
-    existing_user = db.query(User).filter(
-        User.email == user_data.email
-    ).first()
+    existing_user = db.query(User).filter(User.email == user_data.email).first()
 
     if existing_user:
         raise HTTPException(
